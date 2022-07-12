be80c269537e0d55ec9ed2c7934dd302a5477dcc
Asad wiseboxs <asadwiseboxs@gmail.com>
2022-07-12 20:07:59 +0530

    made object literal myObj to push collection of data into array and made add title in html form to add Title to notes

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ showNotes();
 
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
+  let addTitle = document.getElementById("addTitle");
   let addTxt = document.getElementById("addTxt");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
@@ -11,9 +12,15 @@ addBtn.addEventListener("click", function (e) {
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
+  console.log(notesObj);
   addTxt.value = "";
+  addTitle.value="";
   // console.log(notesObj);
   showNotes();
 });
@@ -30,9 +37,9 @@ function showNotes() {
     html += `<div class="noteCard col">
             <div class="card h-100 ">
               <div class="card-body">
-                  <h5 class="card-title">Note ${index + 1}</h5>
+                  <h5 class="card-title">${index+1}. ${element.title} </h5>
                   <p class="card-text">
-                   ${element}
+                   ${element.text}
                   </p>
                   <button id="${index}"onclick="deleteNotes(this.id)" class="btn btn-primary">Delete Note</button>
                </div>
@@ -60,21 +67,18 @@ function deleteNotes(index) {
   showNotes();
 }
 
-
-let search = document.getElementById('searchTxt');
-search.addEventListener("input", function(){
-
-    let inputVal = search.value.toLowerCase();
-    // console.log('Input event fired!', inputVal);
-    let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
-        let cardTxt = element.getElementsByTagName('p')[0].innerText;
-        if(cardTxt.includes(inputVal)){
-            element.style.display = "block";
-        }
-        else{
-            element.style.display = "none";
-        }
-        console.log(cardTxt);
-    })
-})
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function () {
+  let inputVal = search.value.toLowerCase();
+  // console.log('Input event fired!', inputVal);
+  let noteCards = document.getElementsByClassName("noteCard");
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+    console.log(cardTxt);
+  });
+});
