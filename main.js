acc0faad848d27177a3dc9acaf98ad5c7b02dfe9
Asad wiseboxs <asadwiseboxs@gmail.com>
2022-07-12 10:16:40 +0530

    Notes are shown in card view by making showNotes funtion

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
 console.log("welcome to notes project");
 
+showNotes();
+
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function (params) {
+addBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
-  let notesElement = localStorage.getItem("notes");
+  let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  // console.log(notesObj);
+  // showNotes();
 });
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `<div class="col" >
+            <div class="card h-100">
+              <div class="card-body">
+                  <h5 class="card-title">Note ${index + 1}</h5>
+                  <p class="card-text">
+                   ${element}
+                  </p>
+                  <a href="${index}" class="btn btn-secondary">Delete</a>
+               </div>
+            </div>
+            </div>
+                            `;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+  notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+  }
+}
